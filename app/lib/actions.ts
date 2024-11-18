6fe1a2255a391dafafab8ed813cd8b51052e6eb8
Daniel Jaramillo <danieljllo@yahoo.com>
2024-11-18 16:04:01 -0300

    Resolve typescript-eslint/no-unused-vars errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,7 +59,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
-    } catch (error) {
+    } catch {
       // If a database error occurs, return a more specific error.
       return {
         message: 'Database Error: Failed to Create Invoice.',
@@ -96,7 +96,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
         `;
-    } catch (error) {
+    } catch {
         return {
             message: 'Database Error: Failed to Update Invoice.',
         };
@@ -112,7 +112,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
       await sql`DELETE FROM invoices WHERE id = ${id}`;
       revalidatePath('/dashboard/invoices');
       return { message: 'Deleted Invoice' };
-    } catch (error) {
+    } catch {
       return { message: 'Database Error: Failed to Delete Invoice' };
     }
   }
